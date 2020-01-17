3a315b594ae68494cde16bfe2a93bdd693af68cd
Matthias Bilger <matthias@bilger.info>
2020-01-17 14:38:15 +0100

    build message, send at one point

--- bibcheck.py
+++ bibcheck.py
@@ -55,16 +55,17 @@ def check(username, password, notify_ids):
             date = datetime.datetime.strptime(info[1], '%d.%m.%Y')
             delta = date - datetime.datetime.now()
             allinfo.append(str(info))
-            if (delta.days > 7 and delta.days <= 10) or delta.days == 20 or delta.days == 15:
+            if delta.days <= 10 or delta.days == 20 or delta.days == 15:
+                message = f'Bitte an {info[3]} denken\n' 
+                if delta.days <= 7:
+                    message += '<font color="#ff0000">'
+                message += 'Abgabe <b>{info[1]}</b>'
+                if delta.days <= 7:
+                    message += '</font>'
+                message += ' - {username}'
                 for client in itertools.chain(notify_ids, os.environ.get('PUSHOVER_CLIENTS', '').split(',')):
                     try:
-                    	pushover.Client(client).send_message(f'Bitte an {info[3]} denken, Abgabe <b>{info[1]}</b> - {username}', title="Erinnerung", html=1)
-                    except:
-                        print("No client")
-            if delta.days <= 7:
-                for client in itertools.chain(notify_ids, os.environ.get('PUSHOVER_CLIENTS', '').split(',')):
-                    try:
-                    	pushover.Client(client).send_message(f'Bitte an {info[3]} denken, <font color="#ff0000">Abgabe <b>{info[1]}</b></font> - {username}', title="Erinnerung", html=1)
+                        pushover.Client(client).send_message(message, title="Erinnerung", html=1)
                     except:
                         print("No client")
     except (StopIteration, mechanize._mechanize.LinkNotFoundError) as e:
