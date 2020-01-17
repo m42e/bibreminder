41b2b788749369c18440e2115de13c841ed61042
matthias <matthias@bilger.info>
2020-01-17 09:39:08 +0000

    „bibcheck.py“ ändern

--- bibcheck.py
+++ bibcheck.py
@@ -57,7 +57,11 @@ def check(username, password, notify_ids):
             allinfo.append(str(info))
             if delta.days <= 10 or delta.days == 20 or delta.days == 15:
                 for client in itertools.chain(notify_ids, os.environ.get('PUSHOVER_CLIENTS', '').split(',')):
-                    pushover.Client(client).send_message(f'Bitte an {info[3]} denken, Abgabe {info[3]} - {username}', title="Erinnerung")
+                    print("CLIENT {client}")
+                    try:
+                    	pushover.Client(client).send_message(f'Bitte an {info[3]} denken, Abgabe {info[3]} - {username}', title="Erinnerung")
+                    except:
+                        print("No client")
     except (StopIteration, mechanize._mechanize.LinkNotFoundError) as e:
         #for client in itertools.join(notify_ids, os.environ['PUSHOVER_CLIENTS'].split(',')):
         #    pushover.Client(client).send_message(f'nichts ausgeliehen {username} ({e})')
